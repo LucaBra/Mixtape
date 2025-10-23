cbf68d4093a26191fb9188e28a58af7edc4b9cc8
Rafael <123285842+ohey06@users.noreply.github.com>
2025-10-23 12:06:08 -0300

    Update login.js
    Adicionando JWT no login

--- frontend/src/scripts/login.js
+++ frontend/src/scripts/login.js
@@ -1,19 +1,52 @@
-const form = document.getElementById("loginForm");
-const alerta = document.getElementById("alerta");
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("loginForm");
+  const alerta = document.getElementById("alerta");
+  const loginBtn = document.getElementById("loginBtn");
+  const acessarBtn = document.getElementById("acessarBtn");
+  const logoutBtn = document.getElementById("logoutBtn");
+  const success = document.getElementById("success");
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
+  const tokenFake = 
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTIzLCJuYW1lIjoiRW5nLiBDb21wdXRhXHUwMGU3XHUwMGUzbyIsInJvbGUiOiJKV1QiLCJpYXQiOjE3NTk5ODEzMTcsImV4cCI6MTc1OTk4NDkxN30.b1KfcSFInRwYnvRA0Ae5jYuL59KZmCsufPgISNGq0X0";
+ 
+  function validarEmail(email) {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
+}
 
-  // Simulação de validação (substitua por sua lógica real)
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-
-  if (!email || !password) {
-    alerta.textContent = "Preencha todos os campos";
-    alerta.classList.add("show");
-    setTimeout(() => alerta.classList.remove("show"), 3000);
-  } else {
-    // Se login correto, redireciona para a página inicial
-    window.location.href = "../pages/paginaInicial.html";
+  if (form) {
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+    });
   }
+if (loginBtn) {
+  loginBtn.addEventListener("click", () => {
+    const email = document.getElementById("email").value;
+    if (!validarEmail(email)) {
+      alerta.textContent = "E-mail inválido";
+      alerta.classList.add("show");
+      setTimeout(() => alerta.classList.remove("show"), 3000);
+      return;
+    }
+    const senha = document.getElementById("password").value;
+    
+    if (email && senha) {
+      const nomeUsuario = email.split("@")[0];
+      localStorage.setItem("token", tokenFake);
+      localStorage.setItem("nomeUsuario", nomeUsuario);
+      success.textContent = "Login bem-sucedido! Token armazenado.";
+      success.classList.add("show");
+      setTimeout(() => success.classList.remove("show"), 3000);
+      //mensagem.textContent = "Login bem-sucedido! Token armazenado.";
+      window.location.href = "area.html";
+    } else {
+      //mensagem.textContent = "Preencha e-mail e senha.";
+      alerta.textContent = "Preencha e-mail e senha.";
+      alerta.classList.add("show");
+      setTimeout(() => alerta.classList.remove("show"), 3000);
+    }
+  });
+}
 });
+
+
