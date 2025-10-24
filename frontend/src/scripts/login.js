f11f4be8993da85fc1e89c7c105dbb67228c1402
Rafael <rafa2xx6@gmail.com>
2025-10-23 21:19:49 -0300

    Finalização do JS - Login

--- frontend/src/scripts/login.js
+++ frontend/src/scripts/login.js
@@ -2,9 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("loginForm");
   const alerta = document.getElementById("alerta");
   const loginBtn = document.getElementById("loginBtn");
-  const acessarBtn = document.getElementById("acessarBtn");
-  const logoutBtn = document.getElementById("logoutBtn");
-  const success = document.getElementById("success");
+
+
 
   const tokenFake = 
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTIzLCJuYW1lIjoiRW5nLiBDb21wdXRhXHUwMGU3XHUwMGUzbyIsInJvbGUiOiJKV1QiLCJpYXQiOjE3NTk5ODEzMTcsImV4cCI6MTc1OTk4NDkxN30.b1KfcSFInRwYnvRA0Ae5jYuL59KZmCsufPgISNGq0X0";
@@ -14,11 +13,10 @@ document.addEventListener("DOMContentLoaded", () => {
   return regex.test(email);
 }
 
-  if (form) {
-    form.addEventListener("submit", (e) => {
-      e.preventDefault();
-    });
-  }
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
+
 if (loginBtn) {
   loginBtn.addEventListener("click", () => {
     const email = document.getElementById("email").value;
@@ -34,13 +32,17 @@ if (loginBtn) {
       const nomeUsuario = email.split("@")[0];
       localStorage.setItem("token", tokenFake);
       localStorage.setItem("nomeUsuario", nomeUsuario);
+
+      const success = document.getElementById("success");
       success.textContent = "Login bem-sucedido! Token armazenado.";
       success.classList.add("show");
-      setTimeout(() => success.classList.remove("show"), 3000);
-      //mensagem.textContent = "Login bem-sucedido! Token armazenado.";
-      window.location.href = "area.html";
+
+      setTimeout(() => {
+        success.classList.remove("show");
+        window.location.href = "../pages/tape.html";
+      }, 3000);
+
     } else {
-      //mensagem.textContent = "Preencha e-mail e senha.";
       alerta.textContent = "Preencha e-mail e senha.";
       alerta.classList.add("show");
       setTimeout(() => alerta.classList.remove("show"), 3000);
