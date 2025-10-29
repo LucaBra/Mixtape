3ee5dbfc11527c67d3a88646e36f495c3bcb5764
Rafael <rafa2xx6@gmail.com>
2025-10-28 21:02:36 -0300

    fix: more visual fixes

---
+++ frontend/src/scripts/navbar.js
@@ -0,0 +1,33 @@
+const upload = document.getElementById("uploadBtn")
+const profile = document.getElementById("profileBtn")
+
+
+upload.addEventListener("click", function(e) {
+    const token = localStorage.getItem("token");
+    const nomeUsuario = localStorage.getItem("nomeUsuario");
+    if (!token) {
+    window.location.href = "../pages/login.html";
+    return;
+    }
+    window.location.href = "../pages/upload.html";
+}); 
+
+profile.addEventListener("click", function(e) {
+    const token = localStorage.getItem("token");
+    const nomeUsuario = localStorage.getItem("nomeUsuario");
+    if (!token) {
+        window.location.href = "../pages/login.html";
+        return;
+    }
+    window.location.href = "../pages/user.html";
+});
+
+function updateProfileButton() {
+  const token = localStorage.getItem("token");
+  if (token) {
+    const profileUrl = "https://via.placeholder.com/50";
+    profileBtn.innerHTML = `<img src="${profileUrl}" alt="Profile" class="profile-img">`;
+  } else {
+    profileBtn.innerHTML = "Log in";
+  }
+}
--- frontend/src/scripts/tape.js
+++ frontend/src/scripts/tape.js
@@ -1,28 +1,3 @@
-const upload = document.getElementById("uploadBtn")
-const profile = document.getElementById("profileBtn")
-
-
-upload.addEventListener("click", function(e) {
-    const token = localStorage.getItem("token");
-    const nomeUsuario = localStorage.getItem("nomeUsuario");
-    if (!token) {
-    window.location.href = "../pages/login.html";
-    return;
-    }
-    window.location.href = "../pages/upload.html";
-}); 
-
-profile.addEventListener("click", function(e) {
-    const token = localStorage.getItem("token");
-    const nomeUsuario = localStorage.getItem("nomeUsuario");
-    if (!token) {
-        window.location.href = "../pages/login.html";
-        return;
-    }
-    window.location.href = "../pages/user.html";
-});
-
-
 document.querySelectorAll(".card").forEach(card => {
   card.addEventListener("click", () => {
     window.location.href = "../pages/album.html";
